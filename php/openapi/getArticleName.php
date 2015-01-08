81c5a77d7ce33da1f98343170ff3be8a198a8b59
lijiehe <combine.li@corp.globalmarket.com>
2015-01-08 11:05:57 +0800

    2015年1月8日 11:05:53

--- php/openapi/getArticleName.php
+++ php/openapi/getArticleName.php
@@ -1,26 +1,37 @@
-<?php
-error_reporting(0);
+﻿<?php
+header("Content-type: text/html; charset=utf-8");
+
 $dir = "../../app/tpl/article/";  //要获取的目录
 // "********** 获取目录下所有文件和文件夹 ***********<hr/>";
 //先判断指定的路径是不是一个文件夹
-if (is_dir($dir)){
-    if ($dh = opendir($dir)){
-        while (($file = readdir($dh))== true){
-            //文件名的全路径 包含文件名
-            $filePath = $dir.$file;
+if (is_dir($dir)) {
+	if ($dh = opendir($dir)) {
+		while (($file = readdir($dh)) == true) {
 
-                $content = file_get_contents($filePath); //读取文件中的内容
-                $start = strpos($content,'<code>');
-                $end =  strpos($content,'</code>');
-                if( $end - $start != 0){
-                    $num =  $end - $start;
-                    echo substr($content,$start,$num);
-                }
+			if ($file === "." || $file === "..") continue;
+			//文件名的全路径 包含文件名
+			$filePath = $dir . $file;
+			$content = file_get_contents($filePath); //读取文件中的内容
+			//获取类型
+			$start = strpos($content, '<code>')+6;
+			$end = strpos($content, '</code>');
+			$num = $end - $start;
+			$type = substr($content, $start, $num);
+			//获取标题
+			$start = strpos($content, 'data-anchor-id');
+			$start = strpos($content, '>',$start)+1;
+			$end = strpos($content, '</',$start);
+			$num = $end - $start;
+			$title = substr($content, $start, $num);
 
-            //获取文件修改时间
-            $fmt = filemtime($filePath);
-//            echo "<span style='color:#666'>(".date("Y-m-d H:i:s",$fmt).")</span> ".$filePath."<br/>";
-        }
-        closedir($dh);
-    }
+
+			//获取文件修改时间
+			$fmt = filemtime($filePath);
+			$date = date("Y-m-d H:i:s",$fmt);
+			$arrType[$type][] =array("title"=>$title,"updateTime"=>$date);
+		}
+
+		echo json_encode($arrType, JSON_UNESCAPED_UNICODE);
+		closedir($dh);
+	}
 }
